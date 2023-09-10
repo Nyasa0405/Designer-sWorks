a327ba9c98dbaea21b85d6ee21a4b2f9bef694dd
Matuyuhi <Matuyuhi@users.noreply.github.com>
2023-09-10 16:33:05 +0000

    deploy: 668edb22c90868f58eedacef06de3dd72e6b6d2f

---
+++ _next/static/chunks/app/page-b89914b4bc04fe00.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{6470:function(n,e,u){Promise.resolve().then(u.t.bind(u,3222,23))}},function(n){n.O(0,[222,971,596,744],function(){return n(n.s=6470)}),_N_E=n.O()}]);
