decb130558867b35c303325b22b2d5fae02e9fc0
Matuyuhi <bird9.yuhi@gmail.com>
2023-09-11 02:53:26 +0900

    wip

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,4 +1,5 @@
 import {addUrlPrefix} from "@/prefix/prefix";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -16,24 +17,26 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             By{' '}
-            <img
+            <Image
               src={addUrlPrefix("/vercel.svg")}
               alt="Vercel Logo"
               className="dark:invert"
               width={100}
               height={24}
+              priority
             />
           </a>
         </div>
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <img
+        <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src={addUrlPrefix("/next.svg")}
           alt="Next.js Logo"
           width={180}
           height={37}
+          priority
         />
       </div>
 
--- app/src/prefix/prefix.ts
+++ app/src/prefix/prefix.ts
@@ -1,10 +1,5 @@
-import getConfig from "next/config";
-
-const { publicRuntimeConfig } = getConfig();
-
 export function addUrlPrefix(url: string) {
-    const basePath = (publicRuntimeConfig && publicRuntimeConfig.basePath) || "";
-    return basePath
-        ? `/${basePath}${url}`
+    return process.env.URL_PREFIX
+        ? `/${process.env.URL_PREFIX}${url}`
         : url;
 }
